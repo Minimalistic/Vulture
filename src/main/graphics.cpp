e07616b1977733d7fb7f86bab8145c80cae1d080
Dan Stewart <ds191401@gmail.com>
2017-03-03 11:49:37 -0500

    add tinyobjloader as dependency

--- src/main/graphics.cpp
+++ src/main/graphics.cpp
@@ -37,6 +37,9 @@ Window window;
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
 #include "allocator.hpp"
 #include "util.hpp"
 
